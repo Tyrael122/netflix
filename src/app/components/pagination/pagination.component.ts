74e3f33dd7a61a69febc846a54fabff4d520d54d
Kauan Martins Borges <kauan.martins.borges@avanade.com>
2025-06-07 03:49:44 -0300

    working pagination component

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,5 +1,5 @@
 // pagination.component.ts
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter, HostListener} from '@angular/core';
 
 @Component({
   selector: 'netflix-pagination',
