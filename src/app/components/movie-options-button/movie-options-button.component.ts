d82a7834c8188153cbb8e0e7e49c619bd0fe53ad
Kauan Martins Borges <kauan.martins.borges@avanade.com>
2025-06-09 00:57:37 -0300

    Good looking mobile version

--- src/app/components/movie-options-button/movie-options-button.component.ts
+++ src/app/components/movie-options-button/movie-options-button.component.ts
@@ -1,6 +1,5 @@
-import {Component, ElementRef, HostListener, Input, input} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {NetflixIconComponent} from '../netflix-icon/netflix-icon.component';
-import {NgIf} from '@angular/common';
 import {UserMovieListing} from '../../models/movie.model';
 import {SingleDropdownOpenDirective} from '../../directives/single-dropdown-open.directive';
 
@@ -8,7 +7,6 @@ import {SingleDropdownOpenDirective} from '../../directives/single-dropdown-open
   selector: 'netflix-movie-options-button',
   imports: [
     NetflixIconComponent,
-    NgIf,
     SingleDropdownOpenDirective
   ],
   templateUrl: './movie-options-button.component.html',
@@ -17,22 +15,4 @@ import {SingleDropdownOpenDirective} from '../../directives/single-dropdown-open
 export class MovieOptionsButtonComponent {
   movie = input.required<UserMovieListing>()
   size = input<string | number>('20px')
-
-  isMenuOpen: boolean = false;
-
-  constructor(private elementRef: ElementRef) {}
-
-  // Close menu when clicking outside
-  @HostListener('document:click', ['$event'])
-  onClick(event: MouseEvent) {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.isMenuOpen = false;
-    }
-  }
-
-  toggleMenu(event: MouseEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.isMenuOpen = !this.isMenuOpen;
-  }
 }
