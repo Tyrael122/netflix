efe62029def2be03ff695bc0286830167816a970
Kauan Martins Borges <kauan.martins.borges@avanade.com>
2025-07-26 05:44:49 -0300

    Refactor SimilarTabComponent to implement OnInit lifecycle, enhancing permission checks and movie fetching logic

--- src/app/pages/movie-details/components/similar-tab/similar-tab.component.ts
+++ src/app/pages/movie-details/components/similar-tab/similar-tab.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, inject, input} from '@angular/core';
+import {Component, effect, inject, Injector, input, OnInit, runInInjectionContext} from '@angular/core';
 import {MoviePosterImageComponent} from '../../../../components/movie-poster-image/movie-poster-image.component';
 import {MovieListing} from '../../../../models/movie.model';
 import {UserMovieService} from '../../../../services/user/user-movie.service';
@@ -14,22 +14,29 @@ import {RouteParams} from '../../../../enums/app-routes';
   templateUrl: './similar-tab.component.html',
   styleUrl: './similar-tab.component.css'
 })
-export class SimilarTabComponent {
+export class SimilarTabComponent implements OnInit {
   movie = input.required<MovieListing>();
 
   similarMovies: MovieListing[] = [];
   canSeeSimilarMovies = false;
 
+  private injector = inject(Injector);
   private moviesService = inject(UserMovieService);
 
-  constructor() {
-    effect(() => {
-      this.fetchSimilarMovies(this.movie().id);
-    });
-
+  ngOnInit() {
     this.moviesService.hasPermissionToSeeSimilarMovies().subscribe(
-      (hasPermission) => this.canSeeSimilarMovies = hasPermission
-    );
+      (hasPermission) => {
+        runInInjectionContext(this.injector, () => {
+            this.canSeeSimilarMovies = hasPermission;
+
+            if (hasPermission) {
+              effect(() => {
+                this.fetchSimilarMovies(this.movie().id);
+              });
+            }
+          }
+        );
+      });
   }
 
   private fetchSimilarMovies(movieId: string) {
